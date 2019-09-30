9d1ee14f312ad931ae9f89eba9256c27a6badf3d
Raken <hammed.raken123@gmail.com>
2019-09-30 12:55:37 +0100

    fix bug : go to home instead of login when logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './services/authentication-guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
     canActivate: [AuthenticationGuard],
   },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'conversation', loadChildren: './pages/conversation/conversation.module#ConversationPageModule' },
+  {
+    path: 'conversation',
+    loadChildren: './pages/conversation/conversation.module#ConversationPageModule',
+    canActivate: [AuthenticationGuard],
+  },
 ];
 
 @NgModule({
