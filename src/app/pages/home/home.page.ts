a559b670865472d8b7478a5e7af377092851a9d9
Raken <hammed.raken123@gmail.com>
2019-10-15 11:56:03 +0100

    display only other users

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from '../../models/user';
-import { ConversationMessagesService } from 'src/app/services/conversation-messages.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -19,10 +18,10 @@ export class HomePage implements OnInit {
   ) { }
 
   ngOnInit() {
+    const user = JSON.parse(localStorage.getItem('user'));
     this.userService.getUsers().subscribe(
       res => {
-        console.log(res);
-        this.users = res;
+        this.users = res.filter(object => object._id !== user.id);
       },
       err => {
 
